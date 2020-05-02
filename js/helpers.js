9ba088a05a7f38522096fcd79df8d6326fd65c3a
LeandroLuque <lealuque.tw@gmail.com>
2020-05-02 03:37:41 -0300

    fix

--- js/helpers.js
+++ js/helpers.js
@@ -1,6 +1,42 @@
 
 
 
+// function set_limits(data_links){
+
+// 	const transitions = d3.selectAll(".transition")
+
+
+//     var features = ["amplitude", "duration", "avg_velocity", "peak_velocity", "angle"]
+
+//     features.forEach(function(d, i){
+
+//     	var max = d3.max(data_links, l => l[d])
+//     	var min = d3.min(data_links, l => l[d])
+
+   
+// 		max = Math.ceil(max)
+// 	    min = Math.floor(min)
+// 	    step = max-min
+
+// 	    $("#flt_"+d).jRange({
+// 		    from: min,
+// 		    to: max,
+// 		    step: 0.1,
+// 		    scale: d3.range(min, max+step,step),
+// 		    format: '%s',
+// 		    width: 300,
+// 		    showLabels: true,
+// 		    isRange : true,
+// 		    onstatechange: function(value){
+// 		    	var [min, max] = value.split(",")
+// 		    	transitions
+// 		    		.style("display", l => ((l[d]>=min) && (l[d]<=max))?null:"none")
+// 		    }
+// 		});
+
+//     })
+// }
+
 function set_limits(data_links){
 
 	const transitions = d3.selectAll(".transition")
@@ -10,9 +46,15 @@ function set_limits(data_links){
 
     features.forEach(function(d, i){
 
+    	let container_name = "."+d
+
+    	if (d=="angle")
+    		$("."+d).append('<input type="hidden" id="flt_' +d +'" value="-180,180"/>')
+    	else
+    		$("."+d).append('<input type="hidden" id="flt_' +d +'" value="0,1000"/>')
+
     	var max = d3.max(data_links, l => l[d])
     	var min = d3.min(data_links, l => l[d])
-
    
 		max = Math.ceil(max)
 	    min = Math.floor(min)
@@ -28,16 +70,13 @@ function set_limits(data_links){
 		    showLabels: true,
 		    isRange : true,
 		    onstatechange: function(value){
-		    	var [min, max] = value.split(",")
+		    	var [min_l, max_l] = value.split(",")
 		    	transitions
-		    		.style("display", l => ((l[d]>=min) && (l[d]<=max))?null:"none")
+		    		.style("display", l => ((l[d]>=min_l) && (l[d]<=max_l))?null:"none")
 		    }
 		});
 
     })
 
-   
-
-
-
 }
+
